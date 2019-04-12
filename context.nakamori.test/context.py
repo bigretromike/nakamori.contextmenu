04e84ca6f9dc0f244825aa30f640ca03598e0d26
bigretromike <bigretromike@monogatari.pl>
2019-04-12 12:44:16 +0200

    Let's remove TEST, we don't need it anymore;

--- context.nakamori.test/context.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-		
-
-import sys
-import xbmc	
-
-if __name__ == '__main__':		
-    message = "Clicked on '%s'" % sys.listitem.getLabel()
-    xbmc.executebuiltin("Notification(\"Hello context items!\", \"%s\")" % message)
